5d11c8561f2cfdecb6f32832d82abd6632e895e1
Dung-code11 <gamsio204@gmail.com>
2025-06-28 22:53:54 +0700

    dung commit de pull

--- app/Models/SinhVien.php
+++ app/Models/SinhVien.php
@@ -48,7 +48,7 @@ public function tongTinChiDangKy(): int
         ->where('dkmh.trang_thai', 'Đã duyệt')
         ->where('lh.trang_thai', 'Đang mở')
         ->sum('mh.so_tin_chi');
-    return $this->belongsTo(Khoa::class, 'ma_khoa', 'ma_khoa');
+    // return $this->belongsTo(Khoa::class, 'ma_khoa', 'ma_khoa');
 }
 
 public function nganh()
